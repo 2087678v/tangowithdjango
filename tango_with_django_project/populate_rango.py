cc1f6796eceebd2d56334608d0a508baa64ffd20
Ivan <2087678V@student.gla.ac.uk>
2015-01-25 12:27:31 +0000

    Chapter 6 - Completed

--- tango_with_django_project/populate_rango.py
+++ tango_with_django_project/populate_rango.py
@@ -8,7 +8,7 @@ from rango.models import Category, Page
 
 
 def populate():
-    python_cat = add_cat('Python')
+    python_cat = add_cat('Python',128,64)
 
     add_page(cat=python_cat,
         title="Official Python Tutorial",
@@ -22,7 +22,7 @@ def populate():
         title="Learn Python in 10 Minutes",
         url="http://www.korokithakis.net/tutorials/python/")
 
-    django_cat = add_cat("Django")
+    django_cat = add_cat("Django",64,32)
 
     add_page(cat=django_cat,
         title="Official Django Tutorial",
@@ -36,7 +36,7 @@ def populate():
         title="How to Tango with Django",
         url="http://www.tangowithdjango.com/")
 
-    frame_cat = add_cat("Other Frameworks")
+    frame_cat = add_cat("Other Frameworks",32,16)
 
     add_page(cat=frame_cat,
         title="Bottle",
@@ -46,6 +46,15 @@ def populate():
         title="Flask",
         url="http://flask.pocoo.org")
 
+    my_cat = add_cat("2087678v",1024,1023)
+    
+    add_page(cat=my_cat,
+        title = "Github",
+        url="https://github.com/2087678v")
+        
+    add_page(cat=my_cat,
+        title = "Pythonanywhere",
+        url = "http://2087678v.pythonanywhere.com/")
     # Print out what we have added to the user.
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
@@ -55,8 +64,8 @@ def add_page(cat, title, url, views=0):
     p = Page.objects.get_or_create(category=cat, title=title, url=url, views=views)[0]
     return p
 
-def add_cat(name):
-    c = Category.objects.get_or_create(name=name)[0]
+def add_cat(name,views=0,likes=0):
+    c = Category.objects.get_or_create(name=name, views=views, likes=likes)[0]
     return c
 
 # Start execution here!
